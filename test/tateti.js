646ecab51521143055782c93de4558223b44c3b8
unknown <lucasd92@hotmail.com>
2022-04-12 21:34:34 -0300

    Falla sebreescribir casillas

--- test/tateti.js
+++ test/tateti.js
@@ -10,6 +10,7 @@ chai.use(chaiHttp);
 //cuando juega, se ocupa casilla y le toca al segundo
 //cuando juega el segundo, 2 casillas ocupadas con marcas diferentes y le toca al primero
 //Si juega fuera de su turno, el tablero no cambia
+//Si juega en misma casilla, el tablero no cambia y le vuelve a tocar al mismo
 // si hay 3 iguales en una columna,  gana el jugador correspondiente
 // si hay 3 iguales en una fila,  gana el jugador correspondiente
 // si hay 3 iguales en una diagonal, gana el jugador correspondiente
@@ -22,7 +23,7 @@ describe("Juego de TaTeTi", () => {
     let movimientos = [
         { jugador: 'Juan', columna: 0, fila: 0 },
         { jugador: 'Pedro', columna: 1, fila: 0 },
-        { jugador: 'Juan', columna: 0, fila: 1 },
+        { jugador: 'Juan', columna: 1, fila: 1 },
         { jugador: 'Pedro', columna: 1, fila: 1 },
         { jugador: 'Juan', columna: 0, fila: 2 },
     ]
@@ -110,4 +111,27 @@ describe("Juego de TaTeTi", () => {
                 });
         });
     });
+    describe("El primer jugador hace un movimiento en una casilla ocupada", () => {
+        it("El tablero no cambia y recibo el nombre del jugador al que le toca", (done) => {
+            chai.request(server).put("/empezar").send(juego).end();
+            chai.request(server).put("/movimiento").send(movimientos[0]).end();
+            chai.request(server).put("/movimiento").send(movimientos[1]).end();
+            chai.request(server)
+                .put("/movimiento")
+                .send(movimientos[2])
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('turno').eql('Juan');
+                    res.body.should.have.property('tablero').eql([
+                        ['x', 'o', ' '],
+                        [' ', ' ', ' '],
+                        [' ', ' ', ' '],
+                    ]);
+                    done()
+                });
+        });
+    });
+
 });
